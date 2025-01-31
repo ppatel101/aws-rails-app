3467b86519f16602720a96214a780e22cd264865
ppatel101 <parth.logicalstreet@gmail.com>
2025-01-31 11:33:26 +0530

    Update production.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+bind "unix:///home/ubuntu/aws-rails-app/tmp/sockets/puma.sock"
